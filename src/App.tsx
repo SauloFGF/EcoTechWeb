64e31737af50f22f381e391d439853a1542565f3
Saulo Figueiredo <saulo.fgf21@gmai.com>
2024-05-14 00:16:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from './Components/Forms/Button'
+import './Style.css'
+
+
+function App() {
+  return (
+    <>
+    <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Forms/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type ButtonProps = {
+
+}
+
+const Button = () => {
+  return (
+    <button>Button</button>
+  )
+}
+
+export default Button
